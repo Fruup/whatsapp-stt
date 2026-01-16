a0bb4eceaac51be3fa21684dc70e86fc641a2c6c
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2026-01-16 17:33:09 +0100

    init api project

---
+++ api/main.py
@@ -0,0 +1,22 @@
+from faster_whisper import WhisperModel
+
+# model_size = "turbo"
+# model_size = "large-v3"
+model_size = "medium"
+# model_size = "tiny"
+
+# Run on GPU with FP16
+# model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+
+# or run on CPU with INT8
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+segments, info = model.transcribe("../data/test.ogg", beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
